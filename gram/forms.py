5e8ed7c2ea936ab1899cabbc90200e28bc6a4a1b
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-04-02 20:07:32 +0300

    adds forms for login

--- gram/forms.py
+++ gram/forms.py
@@ -19,6 +19,19 @@ class NewUserForm(UserCreationForm):
 			user.save()
 		return user
 
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'location', 'profile_picture', 'bio']
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
