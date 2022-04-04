cf5a113cd45c0f39cef06e918c02231ac2d71673
Tee-Mureithi <esther.mureithi@student.moringaschool.com>
2022-04-04 15:12:30 +0300

    adds files

--- instagram/urls.py
+++ instagram/urls.py
@@ -14,13 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from django.conf.urls import url, include
+from django_registration.backends.one_step.views import RegistrationView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gram.urls'))
+    path('accounts/register/', RegistrationView.as_view(success_url='/'),
+         name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    url(r'^', include('gram.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
